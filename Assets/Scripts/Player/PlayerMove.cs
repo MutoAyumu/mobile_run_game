78671507ca4bf4d91e1571a95366e585dd202539
MutoAyumu <mutoayum0610@gmail.com>
2023-03-08 23:59:01 +0900

    キャラの移動方法を変更

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -5,12 +5,16 @@ public class PlayerMove : MonoBehaviour
 {
     #region 変数
     [Header("Parameter")]
-    [SerializeField] float _horizontalMoveSpeed = 1;
-    [SerializeField] float _verticalMoveSpeed = 3;
+    [SerializeField] float _moveSpeed = 5;
+    [SerializeField] float _rotateSpeed = 500;
+    [SerializeField] float _rotateVelocityLimit = 0.5f;
+    [SerializeField] float _dampSpeed = 0.1f;
 
     Rigidbody _rb;
     Vector2 _dir;
-    float _currentVerticalMoveSpeed;
+    Transform _transform;
+    Animator _anim;
+    float _currentMoveSpeed;
     #endregion
 
     #region プロパティ
@@ -18,14 +22,16 @@ public class PlayerMove : MonoBehaviour
     #endregion
 
     #region 定数
-
+    const string MOVE_ANIM_PARAM = "MoveSpeed";
     #endregion
 
     private void Awake()
     {
         TryGetComponent(out _rb);
+        TryGetComponent(out _transform);
+        TryGetComponent(out _anim);
 
-        _currentVerticalMoveSpeed = _horizontalMoveSpeed;
+        _currentMoveSpeed = _moveSpeed;
 
         InputSystemManager.Instance.MoveVector.Subscribe(OnSetDirection).AddTo(this);
     }
@@ -37,10 +43,32 @@ public class PlayerMove : MonoBehaviour
 
     public void OnMove()
     {
-        var vel = new Vector3(_dir.x, 0, 1).normalized;
-        vel.x *= _horizontalMoveSpeed;
-        vel.y = _rb.velocity.y;
-        vel.z = _currentVerticalMoveSpeed;
-        _rb.velocity = vel;
+        var vel = new Vector3(_dir.x, 0, _dir.y).normalized;
+        var dir = Vector3.zero;
+
+        OnRotate(vel);
+
+        if (vel != Vector3.zero)
+        {
+            dir = _transform.forward * _currentMoveSpeed;
+        }
+
+        dir.y = _rb.velocity.y;
+        _rb.velocity = dir;
+
+        _anim.SetFloat(MOVE_ANIM_PARAM, vel.magnitude, _dampSpeed, Time.deltaTime);
+    }
+
+    void OnRotate(Vector3 vel)
+    {
+        var rot = _transform.rotation;
+        var speed = _rotateSpeed * Time.deltaTime;
+
+        if (vel.magnitude > _rotateVelocityLimit)
+        {
+            rot = Quaternion.LookRotation(vel, Vector3.up);
+        }
+
+        _transform.rotation = Quaternion.RotateTowards(_transform.rotation, rot, speed);
     }
 }
